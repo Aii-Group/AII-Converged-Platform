b7e8d879a6062a1d9d513562dcd343824e90426c
zengxun <zengxun1205@163.com>
2025-04-21 14:39:21 +0800

    feat: Menu persistence

--- src/layouts/components/sidebar.tsx
+++ src/layouts/components/sidebar.tsx
@@ -1,8 +1,8 @@
-import React, { useMemo } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 
 import { Menu, MenuProps } from 'antd'
 import { useTranslation } from 'react-i18next'
-import { renderMenuItems } from '@/utils/system'
+import { findActiveKey, renderMenuItems } from '@/utils/system'
 import menu from '@/utils/menu'
 
 type MenuItem = Required<MenuProps>['items'][number]
@@ -10,14 +10,25 @@ type MenuItem = Required<MenuProps>['items'][number]
 const Sidebar: React.FC = () => {
   const { t, i18n } = useTranslation()
   const menuItems: MenuItem[] = useMemo(() => renderMenuItems(menu, t), [menu, i18n.language])
+  const [selectedKeys, setSelectedKeys] = useState<string[]>([location.pathname])
+
+  useEffect(() => {
+    setSelectedKeys(findActiveKey(menu, location.pathname))
+  }, [menu, location.pathname])
+
+  const handleMenuClick: MenuProps['onClick'] = (e) => {
+    setSelectedKeys([e.key])
+  }
 
   return (
     <div className="sidebar-horizontal">
       <Menu
-        className="!border-0 dark:!border dark:!border-dark-colorBorder"
+        className="!border-0"
         defaultSelectedKeys={['app-1']}
+        selectedKeys={selectedKeys}
         mode="horizontal"
         items={menuItems}
+        onClick={handleMenuClick}
       />
     </div>
   )
